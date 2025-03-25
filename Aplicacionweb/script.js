08044bd0ac070faff79f86618fd9e6a996403e9c
Lucia Hernandez <2az5f6t4k@gmail.com>
2025-03-25 10:58:02 -0600

    Ajustar el package.json

--- Aplicacionweb/script.js
+++ Aplicacionweb/script.js
@@ -9,7 +9,7 @@ function sumar() {
         return;
     }
     //Llamar a la Api
-    fetch('https://apinode-bps7.onrender.com', { ///reemplaza con lla Url de tu API si es diferente
+    fetch('https://sumaapi.onrender.com', { ///reemplaza con lla Url de tu API si es diferente
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
