30b082244c11f922ec1954ade48029c350a33d5a
Lucia Hernandez <2az5f6t4k@gmail.com>
2025-03-25 10:44:54 -0600

    Actualizacion del index

--- Aplicacionweb/script.js
+++ Aplicacionweb/script.js
@@ -9,7 +9,7 @@ function sumar() {
         return;
     }
     //Llamar a la Api
-    fetch('https://apisuma-2h0z.onrender.com', { ///reemplaza con lla Url de tu API si es diferente
+    fetch('https://apinode-bps7.onrender.com', { ///reemplaza con lla Url de tu API si es diferente
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
